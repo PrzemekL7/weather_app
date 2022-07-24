7407576f6c10edcb892219f15f6031fa4453db5c
PrzemekL7 <plehr7@gmail.com>
2022-07-24 15:40:37 +0200

    current weather styled

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import './App.css';
 import Search from "./components/search/Search";
+import CurrentWeather from "./components/currentWeather/CurrentWeather";
 
 function App() {
     const handleOnSearchChange = (searchData) => {
         console.log(searchData);
     };
     return (
-    <div className="container">
-      <Search onSearchChange={handleOnSearchChange} />
-    </div>
-  );
+        <div className="container">
+            <Search onSearchChange={handleOnSearchChange}/>
+            <CurrentWeather/>
+        </div>
+    );
 }
 
 export default App;
